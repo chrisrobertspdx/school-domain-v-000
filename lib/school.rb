d1fd7521c643bf3dd1da4e54edcf8f0f6bdca9b0
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-27 18:54:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,5 +3,15 @@ class School
   attr_reader :name
   def initialize(name) 
     @name = name
+    @roster = {}
+  end
+  def add_student(name,grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+  end
+  def grade(num)
+    @roster[num].join(', ')
   end
 end
